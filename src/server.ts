e608048b3b3f439f25e54276a6f1b6fd120d7309
Raphael Costa <rafa.costa0@hotmail.com>
2024-04-04 05:32:52 -0300

    feature: separate prisma db connection and routes in different files

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,13 @@
 import fastify from "fastify";
-import z from "zod";
-import { PrismaClient } from "@prisma/client";
-import { generateSlug } from "./utils/generate-slug";
+import { serializerCompiler, validatorCompiler, } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify();
 
-const prisma = new PrismaClient({
-  log: ['query'],
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.post('/events', async (request, reply) => {
-  const createEventSchema = z.object({
-    title: z.string().min(4),
-    details: z.string().nullable(),
-    maximumAttendees: z.number().int().positive().nullable(),
-  })
-
-  const {
-    details,
-    maximumAttendees,
-    title
-  } = createEventSchema.parse(request.body);
-
-  const slug = generateSlug(title);
-
-  const eventWithSameSlug = await prisma.event.findUnique({
-    where: {
-      slug,
-    }
-  })
-
-  if (eventWithSameSlug !== null) {
-    throw new Error('Another event with the same title already exists');
-  }
-
-  const event = await prisma.event.create({
-    data: {
-      title,
-      slug,
-      details,
-      maximumAttendees,
-    }
-  })
-
-  // return { eventId: event.id }
-  return reply.status(201).send({ eventId: event.id });
-})
+app.register(createEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
