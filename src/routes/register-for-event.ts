cf6adbe456a09e096b075be5aaef8050b70434fa
Raphael Costa <rafa.costa0@hotmail.com>
2024-04-04 06:10:52 -0300

    feature: add the Promise.all function to register-for-events routes

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -37,17 +37,18 @@ export async function registerForEvent(app: FastifyInstance) {
         throw new Error("This e-mail is already registered for this event.");
       }
 
-      const event = await prisma.event.findUnique({
-        where: {
-          id: eventId,
-        }
-      })
-
-      const amountOfAttendeesForEvent = await prisma.attendee.count({
-        where: {
-          eventId,
-        }
-      });
+      const [event, amountOfAttendeesForEvent] = await Promise.all([
+        prisma.event.findUnique({
+          where: {
+            id: eventId,
+          }
+        }),
+        prisma.attendee.count({
+          where: {
+            eventId,
+          }
+        })
+      ]);
 
       if (event?.maximumAttendees && amountOfAttendeesForEvent >= event?.maximumAttendees) {
         throw new Error("The maximum number of attendees for this event has been reached.");
