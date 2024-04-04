9695d695bf2862bd819ffcdaf6169c3c01bb41ad
Raphael Costa <rafa.costa0@hotmail.com>
2024-04-04 09:50:04 -0300

    feature: add get-attendee-badge response type

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -10,7 +10,16 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         params: z.object({
           attendeeId: z.coerce.number().int(),
         }),
-        response: {}
+        response: {
+          200: z.object({
+            badge: z.object({
+              name: z.string(),
+              email: z.string().email(),
+              eventTitle: z.string(),
+              checkInURL: z.string().url(),
+            })
+          })
+        }
       }
     }, async (request, reply) => {
       const { attendeeId } = request.params;
@@ -34,6 +43,17 @@ export async function getAttendeeBadge(app: FastifyInstance) {
         throw new Error("Attendee not found");
       }
 
-      return reply.send({ attendee });
+      const baseURL = `${request.protocol}://${request.hostname}`
+
+      const checkInURL = new URL(`/attendees/${attendeeId}/check-in`, baseURL)
+
+      return reply.send({
+        badge: {
+          name: attendee.name,
+          email: attendee.email,
+          eventTitle: attendee.event.title,
+          checkInURL: checkInURL.toString(),
+        }
+      });
     });
 }
